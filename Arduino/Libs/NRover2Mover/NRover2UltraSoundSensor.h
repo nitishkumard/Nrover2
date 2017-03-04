583840085f8578185538b97db08a44f73b4ba81d
Nitish Kumar D <nitishkumard@gmail.com>
2017-03-04 19:56:20 +0530

    Adding mover

---
+++ Arduino/Libs/NRover2Mover/NRover2UltraSoundSensor.h
@@ -0,0 +1,18 @@
+#ifndef NRover2Mover_H
+#define NRover2Mover_H
+
+#include <Arduino.h>
+
+class NRover2Mover { 
+public:
+
+	NRover2Mover(HardwareSerial*);
+	~NRover2Mover();  
+	void start();
+	void printSensorDetails();  	
+	void printStatus();
+	int move(int speed);
+	int turn(int degree);
+};
+
+#endif
